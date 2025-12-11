4af430eddcefbac8bdb6251b76eed26a65e06434
Ruth Cerilo <c08.developer@gmail.com>
2025-12-11 13:38:25 +0800

    Adding new file

--- stkpush.php
+++ stkpush.php
@@ -44,4 +44,10 @@ curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
 $curl_response = curl_exec($curl);
 
-echo $curl_response;
+$data = json_decode($curl_response);
+$CheckoutRequestID = $data->CheckoutRequestID;
+$ResponseCode = $data->ResponseCode;
+
+if ($ResponseCode == "0") {
+    echo "The CheckoutRequestID for this transaction is : " . $CheckoutRequestID;
+}
